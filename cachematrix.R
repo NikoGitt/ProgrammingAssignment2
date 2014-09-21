95ee3220a2957f1dbb87d223ffa7f051e8a1328b
NikoGitt <niko_v@live.se>
2014-09-21 13:48:50 +0200

    cacheatrix
    Cachematrix programing assignment 2 Coursera R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The first function, stores the matrix m, and solves the inverse of the matrix through the function setinverse.
+makeCacheMatrix <- function(x = matrix()) { #creates matrix
+  m <- NULL                                 #null the (m)atrix
+  set <- function(y) {                      #set (funtion) stores the x and nulls the m
+    x <<- y          
+    m <<- NULL
+  }
+  get <- function() x                       #get (funtion) returns cached matrix  
+  setsolve <- function(solve) m <<- solve   #setsolve (funtion) stores the (m)atrix in cache
+  getsolve <- function() m                  #getsolve (funtion) returns the cached matrix
+  list(set = set, get = get,                #list(creates the list) containing the set,get,setsolve,getsolve functions
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The second functions, calls the stored m, and if m has changed, recalculates the inverse. 
+cacheSolve <- function(x, ...) {            #creates a function cacheSolve 
+  m <- x$getsolve()                         #x$getsolve returns the cached matrix 
+  if(!is.null(m)) {                         #if(m) checks to see if the (m)atrix has changed  
+    message("getting cached matrix")
+    return(m)
+  }
+  data <- x$get()                           #x$get returns the cached matrix
+  m <- solve(data, ...)                     #(m)atrix inverses the matrix 
+  x$setsolve(m)                             #x$setsolve(function) caches the solved matrix 
+  m                                         #prints the inverse (m)atrix 
 }
+
